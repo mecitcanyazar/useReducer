b8ac67d12bcd1ccf3a1c493d224b2243da16211d
mecitcanyazar <mecitcanyazar@gmail.com>
2022-08-28 14:15:11 +0300

    --

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Setup from './tutorial/3-forms/setup/1-controlled-inputs'
+import Setup from './tutorial/0-useState/setup/3-useState-array'
 // 1-error-example dosyasına Setup ismini verdim ve aşağıda bu componenti return ettim.Artık setup'ı app.js içine aktarmıs oldum.Herhangi baska bir isim de verebilirdim.
 
 
--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export const data = [
-  { id: 1, name: 'murat' },
-  { id: 2, name: 'namık' },
-  { id: 3, name: 'suzan' },
-  { id: 4, name: 'eda' },
+  { id: 1, name: 'Mecit' },
+  { id: 2, name: 'Can' },
+  { id: 3, name: 'Sema' },
+  { id: 4, name: 'Yasemin' },
 ]
