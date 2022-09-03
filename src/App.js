1656dcc187ae7ec9919553c7ac9bd6b22363f098
mecitcanyazar <mecitcanyazar@gmail.com>
2022-09-03 22:57:42 +0300

    --

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Setup from './tutorial/5-useReducer/setup/'
+import Setup from './tutorial/1-useEffect/setup/3-useEffect-fetch-data'
 // 1-error-example dosyasına Setup ismini verdim ve aşağıda bu componenti return ettim.Artık setup'ı app.js içine aktarmıs oldum.Herhangi baska bir isim de verebilirdim.
 
 
